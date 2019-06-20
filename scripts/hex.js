582c97f2869971a0f866a568c60c1bbd86cdc0e7
jawakow <jawakow@gmail.com>
2019-06-20 15:20:18 +0100

    username changed

--- scripts/hex.js
+++ scripts/hex.js
@@ -1,8 +1,16 @@
 $(document).ready(function()
 {
     //window.open("https://www.youtube.com/embed/P9-SUS1j1vE?autoplay=1","SCAM!!!","width = 300 , height = 300");
+    
+    
     $("h1").html("");
     $("h1").append("Let's get some viruses...");
+    
+    var mp3Path = "media\Just Wait a Moment Remix (Kitboga).mp3"
+    var aud1 = document.createElement("audio");
+    aud1.load;
+    aud1.play;
+
     $("#test").html("<h1>Cycling around Drogheda</h1>");
     $("#test").html('<iframe id ="kitboga" src="https://www.youtube.com/embed/P9-SUS1j1vE?autoplay=1" frameborder="0"></iframe>"');
     //$("#kitboga").animate({right: '+=200'}, 5000);
@@ -13,8 +21,5 @@ $(document).ready(function()
      
     // autoplay a mp3 file
 
-    var mp3Path = "media\Just Wait a Moment Remix (Kitboga).mp3"
-    var aud1 = document.createElement("audio");
-    aud1.load;
-    aud1.play;
+    
 });
