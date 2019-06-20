a2e6480a05f096ab198a62cf4e7df65992d5d50c
unknown <lewismac23@hotmail.com>
2019-06-20 10:53:57 +0100

    correction made

--- scripts/BrScripts.js
+++ scripts/BrScripts.js
@@ -3,12 +3,13 @@ $(document).ready(function(){
     function annoyance(){
         window.open("https://www.youtube.com/embed/8sVk34vhYOU","ultrasax", "width =920, height= 680" );
         for(i=0; i <1000; i++){
-            let tempdiv = ("<div>",{"class":"knacker"});
-            let wordOne = ("<h1>").append("FOR ");
-            let wordTwo = ("<h3>").append("THE ");
-            let wordThree = ("<h2>").append("EMPEROR!!!");
+            let tempdiv = $("<div>",{"class":"knacker"});
+            let wordOne = $("<h1>").append("FOR ");
+            let wordTwo = $("<h3>").append("THE ");
+            let wordThree = $("<h2>").append("EMPEROR!!!");
             $(tempdiv).append(wordOne, wordTwo, wordThree);
             $(".injection").append(tempdiv);
+            console.log(tempdiv);
         }
     }
 
