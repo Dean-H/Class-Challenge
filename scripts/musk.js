c8349bcb7d2fbf843b64e30f461952c3440a6f19
lynnC <lynncassidy91@gmail.com>
2019-06-20 10:46:54 +0100

    working-animation

---
+++ scripts/musk.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $("#animated-text").mouseenter(function(){
+        console.log("hello")
+        $("#animated-text").animate({fontSize:"60px"},"fast");
+        $("#animated-text").animate({fontSize:"20px"},"slow");
+    });
+
+
+});
