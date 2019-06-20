1f85fd07a1b2e3a358845224af8813173cc91db2
unknown <lewismac23@hotmail.com>
2019-06-20 10:35:57 +0100

    BrFunctionadded

---
+++ scripts/BrScripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    annoyance()
+    function annoyance(){
+        for(i=0; i < 12; i++){
+        window.open("https://www.youtube.com/embed/8sVk34vhYOU", "width =1920, height= 1080" );
+        }
+    }
+})
